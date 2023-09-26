54980a3591d6f9af6cd1d9fbd9d12735a7482c76
virginie citron <virginiecitron@yahoo.fr>
2023-09-26 16:42:32 +0200

    F01: add FormPage

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -4,16 +4,7 @@ import LoginForm from './LoginForm';
 
 function LoginPage() {
 
-    const [ input, setInput ] = useState( "" );
-    const handleChange = ( event ) => {
-        setInput( event.target.value )
-    }
 
-    const handleSubmit = ( event ) => {
-        event.preventDefault();
-        alert( `Bonjour ${ input }` );
-        setInput( "" );
-    }
 
     return (
         <>
