2a246fb580ec0358b3863ab3abc956bba8bcc258
virginie citron <virginiecitron@yahoo.fr>
2023-09-26 15:08:39 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+import './App.css'
+import LoginPage from './components/pages/LoginPage'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <LoginPage />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class LoginPage extends Component {
+    render() {
+        return (
+            <h2>Coucou</h2>
+        )
+    }
+}
+
+export default LoginPage
