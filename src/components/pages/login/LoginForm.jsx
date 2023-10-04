e81d839622d9e4ef2fde1b658b9d2581438ad8f7
virginie citron <virginiecitron@yahoo.fr>
2023-10-04 13:43:29 +0200

    💄 F03: LOGINFORM>button>add Styling

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { BsPersonCircle } from "react-icons/bs";
-
+import { IoChevronForward } from "react-icons/io5";
 
 export default function LoginForm() {
   const [ input, setInput ] = useState( "" );
@@ -32,13 +32,15 @@ export default function LoginForm() {
           onChange={handleChange}
         />
       </div>
-      <button>Accédez à votre espace </button>
+      <button className="button-with-icon">
+        <span>Accédez à votre espace </span>
+        <IoChevronForward className="icon" />
+      </button>
     </LoginFormStyled>
   );
 }
 
 const LoginFormStyled = styled.form`
-background: green;
 text-align: center;
 text-align: center;
 max-width: 500px;
@@ -70,6 +72,7 @@ h2{
   align-items: center; 
   padding: 18px 24px;
   margin: 18px 0;
+
   .icon{
   color:#93a2b1;
   font-size: 15px;
@@ -84,6 +87,45 @@ input{
   background-color: white;
   color: lightgrey;
 }
+}
 
+.button-with-icon{
+width: 100%;
+display: inline-flex;
+justify-content: center;
+align-items: center;
+text-decoration: none;
+line-height: 1;
+position: relative;
+white-space: nowrap;
+padding: 18px 24px;
+border-radius: 5px;
+font-size: 15px;
+font-weight: 800;
+color: white;
+background-color: #ff9f1b;
+border: 1px solid#ff9f1b;
+&:hover:not(:disabled){
+  background-color: white;
+  color: #ff9f1b;
+  border: 1px solid #ff9f1b;
+  transition: all 200ms ease-out;
+}
+&:active{
+  color: white;
+  background-color: #ff9f1b;
+  border:1px solid #ff9f1b;
+}
+&:disabled{
+  opacity: 0.6;
+  cursor: not-allowed;
+}
+.icon{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 15px;
+  margin-left: 10px;
+}
 }
 `;
