6106ea4abcb8d800daf6030f979163005de79aec
virginie citron <virginiecitron@yahoo.fr>
2023-10-06 15:16:11 +0200

    🚧 F04: ORDER>layout>navbar>add left-side and write side

--- src/components/pages/login/order/Navbar.jsx
+++ src/components/pages/login/order/Navbar.jsx
@@ -7,9 +7,15 @@ export default function Navbar( { username } ) {
 
     return (
         <NavbarStyled>
-            <h1>Bonjour {username}</h1>
-            <Link to="/">
-                <button>Déconnexion</button></Link>
+            <div className="left-side">Left Side</div>
+            <div className="right-side">
+                Right-side
+                <h1>Bonjour {username}</h1>
+                <Link to="/">
+                    <button>Déconnexion</button>
+                </Link>
+            </div>
+
         </NavbarStyled>
     )
 }
@@ -17,4 +23,19 @@ export default function Navbar( { username } ) {
 const NavbarStyled = styled.nav`
 background: blue;
 height: 10vh;
+display: flex;
+justify-content: space-between;
+align-items: center;
+
+
+.left-side{
+background: pink;
+}
+
+.right-side{
+
+background: violet;
+}
+
+
 `;
