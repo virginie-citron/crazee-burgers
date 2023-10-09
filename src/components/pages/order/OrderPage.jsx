f61f144dda81192dd2b53d90e6627075afc0d4c4
virginie citron <virginiecitron@yahoo.fr>
2023-10-09 16:21:26 +0200

    💄 F04: ORDER> add primary color to background color

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -22,7 +22,7 @@ export default function OrderPage() {
 
 
 const OrderpageStyled = styled.div`
-background: orange;
+background:${ theme.colors.primary };
 height: 100vh;
 display: flex;
 justify-content: center;
