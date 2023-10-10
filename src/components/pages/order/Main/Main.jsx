4343c0b7cd8eaf0364ca11e6d39a5173779fea4a
virginie citron <virginiecitron@yahoo.fr>
2023-10-10 15:44:52 +0200

    🚧 F05: ORDER>MENU>Add Grid Layout

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -21,6 +21,7 @@ border-bottom-right-radius:${ theme.borderRadius.extraRound };
 box-shadow: 0px 8px 20px 8px rgba(0,0,0,0.2) inset;
 display:grid;
 grid-template-columns: 25% 1fr;
+overflow-y: scroll;
 
 
 
--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components';
+import { fakeMenu2 } from '../../../../fakeData/fakeMenu';
 
 export default function Menu() {
+    const [ menu, setMenu ] = useState( fakeMenu2 )
     return (
         <MenuStyled>
-            Menu
+            {menu.map( ( element ) => { return <div className='produit'>{element.title}</div> } )}
         </MenuStyled>
     )
 }
-
 const MenuStyled = styled.div`
 background: green;
-  
+display: grid;
+grid-template-columns:repeat(4,1fr);
+grid-row-gap:60px ;
+padding:50px 50px 150px;
+justify-items: center;
+
+
+.produit{
+    background: red;
+    width: 240px;
+    height: 330px;
+}
 `;
